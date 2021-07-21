0acdbffeb7a887d358ebc8c4147b417b3d054735
Joshua Simon <ga.simonj@gmail.com>
2021-07-21 15:36:22 -0400

    added alert feature / removed card background demension

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import './MemoryCard.css';
 
+
+
 class MemoryCard extends React.Component {
+    clickHandler(){
+        alert("card clicked")};
     render() {
-        return <div className="MemoryCard">
-            <div className="MemomoryCardInner">
+        return <div className="MemoryCard" onClick={this.clickHandler}>
+            <div className="MemoryCardInner">
                 <div className="MemoryCardBack"> <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" /></div>
 
                 <div className="MemoryCardFront"> ∆ </div>
