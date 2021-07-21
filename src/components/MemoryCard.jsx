a87844a48367684de9a526483ab7c0d4d0a28a2c
Joshua Simon <ga.simonj@gmail.com>
2021-07-21 14:59:49 -0400

    deleted old css files

--- src/components/MemoryCard.jsx
+++ src/components/MemoryCard.jsx
@@ -1,11 +1,11 @@
 import React from "react";
-import './MemoryCardBack.css';
+import './MemoryCard.css';
 
 class MemoryCard extends React.Component {
     render() {
         return <div className="MemoryCard">
             <div className="MemomoryCardInner">
-                <div className="MemoryCardBack"> <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" />Memory Card</div>
+                <div className="MemoryCardBack"> <img src="https://www.digitalcrafts.com/img/digitalcrafts-logo-white-y.png" /></div>
 
                 <div className="MemoryCardFront"> ∆ </div>
             </div>
